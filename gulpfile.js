c9869b7013a6f9950e27af77850d2bd56c2642b7
Pieter Willem Jordaan <pieterwjordaanpc@gmail.com>
2023-08-11 10:21:19 +0200

    Upgrade deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,53 @@
-var
-	gulp = require('gulp'),
-	concat = require('gulp-concat'),	
-	rename = require('gulp-rename'),	
-	cssmin = require('gulp-cssmin'),	
-	sass = require('gulp-sass'),	
-	autoprefixer = require('gulp-autoprefixer'),	
-	uglify = require('gulp-uglify'),	
-	sourcemaps = require('gulp-sourcemaps')
-	gzip = require('gulp-gzip')
-;
-
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    cssmin = require('gulp-cssmin'),
+    sass = require('gulp-sass')(require('node-sass')),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    gzip = require('gulp-gzip');
 
 // Build JavaScript
-gulp.task('buildJS', function(){
-	gulp
-	.src('src/js/**/*.js')
-	.pipe(concat('ng-dropzone.js'))
-	.pipe(sourcemaps.init())
-	.pipe(gulp.dest('dist'))
-	.pipe(rename({suffix:'.min'}))
-	.pipe(uglify())
-	.pipe(sourcemaps.write('./'))
-	.pipe(gulp.dest('dist'))
-	.pipe(gzip({append:true}))
-	.pipe(gulp.dest('dist'))
-});
+function buildJS() {
+    return gulp
+        .src('src/js/**/*.js')
+        .pipe(concat('ng-dropzone.js'))
+        .pipe(sourcemaps.init())
+        .pipe(gulp.dest('dist'))
+        .pipe(rename({suffix:'.min'}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'))
+        .pipe(gzip({append:true}))
+        .pipe(gulp.dest('dist'));
+}
 
 // Build SASS
-gulp.task('buildSASS', function(){
-	gulp
-	.src('src/sass/**/*.scss')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(concat('ng-dropzone.css'))
-	.pipe(autoprefixer())
-	.pipe(sourcemaps.init())
-	.pipe(gulp.dest('dist'))
-	.pipe(rename({suffix:'.min'}))
-	.pipe(cssmin())
-	.pipe(sourcemaps.write('./'))
-	.pipe(gulp.dest('dist'))
-	.pipe(gzip({append:true}))
-	.pipe(gulp.dest('dist'))
-});
-
-// Build all
-gulp.task('build', ['buildJS', 'buildSASS']);
+function buildSASS() {
+    return gulp
+        .src('src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('ng-dropzone.css'))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.init())
+        .pipe(gulp.dest('dist'))
+        .pipe(rename({suffix:'.min'}))
+        .pipe(cssmin())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'))
+        .pipe(gzip({append:true}))
+        .pipe(gulp.dest('dist'));
+}
 
 // Watch all
-gulp.task('watch', ['build'], function(){
-	gulp.watch('src/js/**/*.js', ['buildJS']);
-	gulp.watch('src/sass/**/*.scss', ['buildSASS']);
-});
+function watch() {
+    gulp.watch('src/js/**/*.js', buildJS);
+    gulp.watch('src/sass/**/*.scss', buildSASS);
+}
+
+// Define tasks
+gulp.task('buildJS', buildJS);
+gulp.task('buildSASS', buildSASS);
+gulp.task('build', gulp.parallel('buildJS', 'buildSASS')); // use gulp.parallel or gulp.series as needed
+gulp.task('watch', gulp.series('build', watch)); // watch depends on build, so use gulp.series
